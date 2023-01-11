566c37fe5b5adb634b096972cffa1fa3fce23615
Jfutkhkhjgh <108340300+Sjshskdhd@users.noreply.github.com>
2023-01-11 20:01:12 +0200

    Update _chatbot.js

--- plugins/_chatbot.js
+++ plugins/_chatbot.js
@@ -1,11 +1,11 @@
 let handler  = async (m, { conn }) => {
 let name = conn.getName(m.sender)
 let teks = `
-${pickRandom([` ايش بدك `, `مش فاضيه لك`, `_خلصني قول وش بدك_`, `_ها حياتي_`, '  يونا دام في خدمتك😄'])}
+${pickRandom([` ايش بدك `, `مش فاضيه لك`, `_خلصني قول وش بدك_`, `_ها حياتي_`, '  الجزار دائما في خدمتك😄'])}
 `.trim()
 conn.reply(m.chat, teks, m, { mentions: { mentionedJid: [m.sender] }})
 }
-handler.customPrefix = /يونا وينك|يونا|وين يونا/i
+handler.customPrefix = /جزار وينك|جزار|وين الجزار/i
 handler.command = new RegExp
 
 export default handler
