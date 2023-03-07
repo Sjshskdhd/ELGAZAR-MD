12507cf5b84cad3c4f0541b4874dc993fbe235c5
Jfutkhkhjgh <108340300+Sjshskdhd@users.noreply.github.com>
2023-03-07 22:35:54 +0200

    Delete plugins directory

--- plugins/herramientas-qrcode.js
+++
@@ -1,9 +0,0 @@
-import { toDataURL } from 'qrcode'
-let handler = async (m, { text, conn }) => {
-if (!text) throw `${mg} اكتب نصا لتحويله إلى رمز الاستجابة السريعة`
-conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', wm, m)
-}
-handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
-handler.tags = ['tools']
-handler.command = /^qr(code)?$/i
-export default handler
--- plugins/jadibot-finalizar.js
+++
@@ -1,9 +0,0 @@
-let handler  = async (m, { conn }) => {
-if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, `${ag} إذا لم تكن روبوتا فرعيا ، فاتصل بالرقم الرئيسي لتصبح روبوتا`, m)
-else {
-await conn.reply(m.chat, `${rg} لقد قمت بتسجيل الخروج معي 😯`, m)
-conn.ws.close()}}
-handler.command = /^(berhenti|stop|detener)$/i
-handler.owner = true  
-handler.fail = null
-export default handler
